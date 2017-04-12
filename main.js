cc042c784316f36ebed8cb378a570f28f35b37a8
Grant Denmark <grantdenmark@mac.local>
2017-04-11 20:43:25 -0400

    qw

--- main.js
+++ main.js
@@ -6,11 +6,35 @@ $(document).ready(function() {
     FB.init({
       appId: '913782388761480',
       version: 'v2.7' // or v2.1, v2.2, v2.3, ...
-    });     
+    });
     $('#loginbutton,#feedbutton').removeAttr('disabled');
     FB.getLoginStatus(updateStatusCallback);
 	  console.log(FB);
   });
+  {
+      "images": [
+          {
+              "filename": "ic_contacts.png",
+              "idiom": "universal",
+              "scale": "1x"
+          },
+          {
+              "filename": "ic_contacts_2x.png",
+              "idiom": "universal",
+              "scale": "2x"
+          },
+          {
+              "filename": "ic_contacts_3x.png",
+              "idiom": "universal",
+              "scale": "3x"
+          }
+      ],
+      "info": {
+          "author": "xcode",
+          "version": 1
+      }
+  }
+
 
 });
 
@@ -24,5 +48,5 @@ function view(v){
 }
 
 function updateStatusCallback(){
-	
+
 }
